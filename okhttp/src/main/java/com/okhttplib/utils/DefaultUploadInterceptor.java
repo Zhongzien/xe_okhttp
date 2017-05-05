a53c86abddfb26dd9fa5ef4280ec4719bd9c715a
wind <394583465@qq.com>
2017-05-05 17:30:11 +0800

    v2.0 新增文件上传，支持多格式文件上传 上传方式提供同步/异步两种，同时均支持单文件/多文件上传

--- okhttp/src/main/java/com/okhttplib/utils/DefaultUploadInterceptor.java
+++ okhttp/src/main/java/com/okhttplib/utils/DefaultUploadInterceptor.java
@@ -12,26 +12,30 @@ public class DefaultUploadInterceptor implements MediaTypeInterceptor {
     @Override
     public String intercept(String filePath) {
 
-        if (!TextUtils.isEmpty(filePath) && filePath.contains(".")) {
-            String suffix = filePath.substring(filePath.indexOf(".") + 1);
-            if ("png".equals(suffix)) {
-                suffix = "image/png";
-            } else if ("jpg".equals(suffix)) {
-                suffix = "image/jpg";
-            } else if ("jpeg".equals(suffix)) {
-                suffix = "image/jpeg";
-            } else if ("ioc".equals(suffix)) {
-                suffix = "image/ico";
-            } else if ("gif".equals(suffix)) {
-                suffix = "image/gif";
-            } else if ("bmp".equals(suffix)) {
-                suffix = "image/bmp";
+        if (!TextUtils.isEmpty(filePath)) {
+            if(filePath.contains(".")){
+                String suffix = filePath.substring(filePath.indexOf(".") + 1);
+                if ("png".equals(suffix)) {
+                    suffix = "image/png";
+                } else if ("jpg".equals(suffix)) {
+                    suffix = "image/jpg";
+                } else if ("jpeg".equals(suffix)) {
+                    suffix = "image/jpeg";
+                } else if ("ioc".equals(suffix)) {
+                    suffix = "image/ico";
+                } else if ("gif".equals(suffix)) {
+                    suffix = "image/gif";
+                } else if ("bmp".equals(suffix)) {
+                    suffix = "image/bmp";
+                } else {
+                    suffix = "multipart/form-data";
+                }
+                return suffix;
             } else {
-                suffix = "multipart/form-data";
+                return "application/octet-stream";
             }
-            return suffix;
         }
 
-        return null;
+        return "";
     }
 }
